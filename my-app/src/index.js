6f943196e924cce596072254f07318996ce7931d
Darah Umadac <darahfumadac@gmail.com>
2021-06-09 22:09:59 +0800

    react component jsx basics

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,26 @@
+// Import react and reactDom libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// function getButtonText() {
+//   return 'Click on me!!!';
+// }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Create a react component
+const App = () => {
+  const buttonText = { text: 'Click me!' };
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
+};
+
+// Show react component on the screen
+ReactDOM.render(<App />, document.getElementById('root'));
