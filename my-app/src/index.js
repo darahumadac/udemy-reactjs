9b57ce1a99fcb02c04e7d8430f069857ce4c1559
Darah Umadac <darahfumadac@gmail.com>
2021-06-15 01:41:41 +0800

    component with props

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div className="ui container comments">
+      <CommentDetail
+        profilePic={faker.image.avatar()}
+        author="Sam"
+        timeAgo="Today at 4:50PM"
+        comment="Nice"
+      />
+      <CommentDetail
+        profilePic={faker.image.avatar()}
+        author="Jane"
+        timeAgo="Today at 2:00AM"
+        comment="Ok"
+      />
+      <CommentDetail
+        profilePic={faker.image.avatar()}
+        author="Alex"
+        timeAgo="Yesterday at 5:00PM"
+        comment="Nope"
+      />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
