172c5a9938d97b96ba6cc0f6baed785e57c7844a
Darah Umadac <darahfumadac@gmail.com>
2021-06-15 00:57:21 +0800

    use semantic ui, new file for components study

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,26 +1,8 @@
-// Import react and reactDom libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// function getButtonText() {
-//   return 'Click on me!!!';
-// }
-
-// Create a react component
 const App = () => {
-  const buttonText = { text: 'Click me!' };
-  return (
-    <div>
-      <label className="label" htmlFor="name">
-        Enter name:
-      </label>
-      <input id="name" type="text" />
-      <button style={{ backgroundColor: 'blue', color: 'white' }}>
-        {buttonText.text}
-      </button>
-    </div>
-  );
+  return <div>Hi there!</div>;
 };
 
-// Show react component on the screen
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
